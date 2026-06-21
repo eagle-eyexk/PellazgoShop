7babd9803888536d89be5473cab55fa1ba49a39c
EagleEye <agentextern@gmail.com>
2026-06-21 15:30:51 +0200

    init: add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>PellazgoShop</h1>
+      <p>Your app is ready. Start editing <code>src/App.jsx</code></p>
+    </div>
+  );
+}
